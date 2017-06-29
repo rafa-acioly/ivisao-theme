b4136270b6d14e5b9b73b008b959762c14fecc80
Rafael <aciolyr@gmail.com>
2017-06-29 14:53:04 -0300

    layout home finalizado

--- components/nav.php
+++ components/nav.php
@@ -1,4 +1,4 @@
-<nav class="nav has-shadow">
+<nav class="nav">
   <div class="container">
     <div class="nav-left">
       <div class="nav-item">
@@ -8,45 +8,31 @@
 
     <div class="nav-right">
       <div class="nav-menu-contact">
-        <div class="nav-item">
+        <div class="nav-item is-hidden-mobile">
           <span class="icon"><i class="fa fa-phone fa-fw"></i></span>
           12 3846.7888
         </div>
-        <div class="nav-item">
+        <div class="nav-item is-hidden-mobile">
           <span class="icon"><i class="fa fa-whatsapp fa-fw"></i></span>
           12 99750.7888
         </div>
-        <div class="nav-item">
+        <div class="nav-item is-hidden-mobile">
           <span class="icon"><i class="fa fa-whatsapp fa-fw"></i></span>
           12 99767.7888
         </div>
-        <div class="nav-item">
-          <span class="icon"><i class="fa fa-envelope"></i></span>
+        <div class="nav-item is-hidden-mobile">
+          <span class="icon"><i class="fa fa-envelope fa-fw"></i></span>
           contato@ivisaosjc.com.br
         </div>
       </div>
       <div class="nav-menu-itens">
-        <div class="nav-item is-tab">
-          <a href="#">Empresas</a>
-        </div>
-        <div class="nav-item is-tab">
-          <a href="#">Exames</a>
-        </div>
-        <div class="nav-item is-tab">
-          <a href="#">Cirurgias</a>
-        </div>
-        <div class="nav-item is-tab">
-          <a href="#">Para Médicos</a>
-        </div>
-        <div class="nav-item is-tab">
-          <a href="#">Corpo Clínico</a>
-        </div>
-        <div class="nav-item is-tab">
-          <a href="#">Contato</a>
-        </div>
-        <div class="nav-item is-tab">
-          <a href="#">Pré-agendamento</a>
-        </div>
+        <a class="nav-item is-tab is-active">Empresas</a>
+        <a class="nav-item is-tab">Exames</a>
+        <a class="nav-item is-tab">Cirurgias</a>
+        <a class="nav-item is-tab">Para Médicos</a>
+        <a class="nav-item is-tab">Corpo Clínico</a>
+        <a class="nav-item is-tab">Contato</a>
+        <a class="nav-item is-tab">Pré-agendamento</a>
       </div>
     </div>
   </div>
--- index.php
+++ index.php
@@ -5,13 +5,65 @@
     <?php get_template_part('components/nav', 'nav') ?>
   </div>
 
-  <div class="hero-body">
-    
-  </div>
+  <div class="body">
+    <figure>
+      <img src="http://placehold.it/1920x500" alt="">
+      <figcaption class="is-fullwidth">
+        <h1 class="title is-2">
+          <span>Uma clínica completa</span> <br>
+          para o tratamento <br>
+          dos seus olhos
+        </h1>
 
-  <div class="hero-foot">
-
+        <div class="banner-menu">
+          <div class="item has-text-centered">
+            <a href="#">
+              <img src="https://source.unsplash.com/random/150x150" alt="" class="is-round">
+            </a>
+            <h1 class="title is-4">Médicos</h1>
+            <h2 class="subtitle">Área restrira para profissionais</h2>
+          </div>
+          <div class="item has-text-centered">
+            <a href="#">
+              <img src="https://source.unsplash.com/random/150x150" alt="" class="is-round">
+            </a>
+            <h1 class="title is-4">Pré-agendamento</h1>
+            <h2 class="subtitle">Consultas e exames</h2>
+          </div>
+        </div>
+      </figcaption>
+    </figure>
   </div>
+  <div class="hero-footer">
+    <div class="container">
+      <div class="columns">
+        <div class="column vertical-align">
+          <h1 class="title is-4">
+            A principal clinica para atendimentos <br>
+            e procedimentos oftalmológicos <br>
+            na região do Vale do Paraíba.
+          </h1>
+          <a href="" class="button is-info">Clique e conheça mais</a>
+        </div>
+        <div class="column has-text-right vertical-align">
+          <h1 class="title">Conviênios</h1>
+        </div>
+        <div class="column">
+          <img src="http://placehold.it/90x90" alt="" class="is-round">
+          <img src="http://placehold.it/90x90" alt="" class="is-round">
+          <img src="http://placehold.it/90x90" alt="" class="is-round">
+          <img src="http://placehold.it/90x90" alt="" class="is-round">
+          <img src="http://placehold.it/90x90" alt="" class="is-round">
+          <img src="http://placehold.it/90x90" alt="" class="is-round">
+          <img src="http://placehold.it/90x90" alt="" class="is-round">
+          <img src="http://placehold.it/90x90" alt="" class="is-round">
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="hero-foot has-text-centered">
+    Avenida Tivoli, 279, Jd.Bethania - São José dos Campos
+  </footer>
 </div>
 
 <?php get_footer(); ?>
