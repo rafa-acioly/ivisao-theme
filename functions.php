b3b311c64c9ea8124dac2861cfe37e86adceced8
Rafael <aciolyr@gmail.com>
2017-06-29 17:44:42 -0300

    Editor para pagina estatica incluido

--- functions.php
+++ functions.php
@@ -2,20 +2,51 @@
 
 add_theme_support( 'menus' );
 
-// Clear Menu Side
-function clean_menu() {
+/**
+ * Remove opções do dashboard principal
+ */
+ function clean_menu() {
   global $menu;
   global $submenu;
   unset($submenu['edit.php'][16]);
-  remove_menu_page ('upload.php');
-  remove_menu_page ('link-manager.php');
-  remove_menu_page ('edit.php?post_type=page');
-  // remove_menu_page ('options-general.php');
-  remove_menu_page ('tools.php');
-  remove_menu_page ('edit.php');
-  remove_menu_page ('edit-comments.php');
-  // remove_menu_page ('plugins.php');
-  remove_menu_page ('themes.php');
-  remove_menu_page ('users.php');
+  remove_menu_page('upload.php');
+  remove_menu_page('link-manager.php');
+  remove_menu_page('edit.php?post_type=page');
+  // remove_menu_page('options-general.php');
+  remove_menu_page('tools.php');
+  // remove_menu_page('edit.php');
+  remove_menu_page('edit-comments.php');
+  // remove_menu_page('plugins.php');
+  //remove_menu_page('themes.php');
+  remove_menu_page('users.php');
+ }
+ add_action('admin_menu', 'clean_menu');
+
+/**
+ * Remove opções do navbar do dashboard principal
+ */
+function clean_bar() {
+  global $wp_admin_bar;
+  $wp_admin_bar->remove_menu('wp-logo');
+  $wp_admin_bar->remove_menu('comments');
+  $wp_admin_bar->remove_node('new-media');
+  $wp_admin_bar->remove_node('new-user');
+  $wp_admin_bar->remove_menu('edit-profile');
 }
-add_action ('admin_menu', 'clean_menu');
+add_action( 'wp_before_admin_bar_render', 'clean_bar' );
+
+/**
+ * Adiciona um menu no dashboard para inserir textos de paginas estaticas
+ */
+function wpdocs_register_my_custom_menu_page() {
+   add_menu_page(
+      null,
+      'Conteúdo Geral',
+      'edit_posts',
+      '/post.php?post=2&action=edit',
+      null,
+      'dashicons-media-default',
+      2
+  );
+}
+add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
