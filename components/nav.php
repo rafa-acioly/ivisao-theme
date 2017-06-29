ab22e6b49bd364649ff8ac98fe0ea8928a83c7e1
Rafael <aciolyr@gmail.com>
2017-06-29 11:59:37 -0300

    opções de navbar incluidas com icones

--- components/nav.php
+++ components/nav.php
@@ -7,13 +7,47 @@
     </div>
 
     <div class="nav-right">
-      <div class="nav-item is-tab is-active">1</div>
-      <div class="nav-item is-tab">2</div>
-      <div class="nav-item is-tab">3</div>
-      <div class="nav-item is-tab">4</div>
-      <div class="nav-item is-tab">5</div>
-      <div class="nav-item is-tab">6</div>
-      <div class="nav-item is-tab">7</div>
+      <div class="nav-menu-contact">
+        <div class="nav-item">
+          <span class="icon"><i class="fa fa-phone fa-fw"></i></span>
+          12 3846.7888
+        </div>
+        <div class="nav-item">
+          <span class="icon"><i class="fa fa-whatsapp fa-fw"></i></span>
+          12 99750.7888
+        </div>
+        <div class="nav-item">
+          <span class="icon"><i class="fa fa-whatsapp fa-fw"></i></span>
+          12 99767.7888
+        </div>
+        <div class="nav-item">
+          <span class="icon"><i class="fa fa-envelope"></i></span>
+          contato@ivisaosjc.com.br
+        </div>
+      </div>
+      <div class="nav-menu-itens">
+        <div class="nav-item is-tab">
+          <a href="#">Empresas</a>
+        </div>
+        <div class="nav-item is-tab">
+          <a href="#">Exames</a>
+        </div>
+        <div class="nav-item is-tab">
+          <a href="#">Cirurgias</a>
+        </div>
+        <div class="nav-item is-tab">
+          <a href="#">Para Médicos</a>
+        </div>
+        <div class="nav-item is-tab">
+          <a href="#">Corpo Clínico</a>
+        </div>
+        <div class="nav-item is-tab">
+          <a href="#">Contato</a>
+        </div>
+        <div class="nav-item is-tab">
+          <a href="#">Pré-agendamento</a>
+        </div>
+      </div>
     </div>
   </div>
 </nav>
