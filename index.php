2caac0d0de0d116f31c792abef698aaace9054d9
Rafael <aciolyr@gmail.com>
2017-07-07 17:34:44 -0300

    icones de banner da home removidos temporariamente e link em conheça mais incluido

--- index.php
+++ index.php
@@ -15,12 +15,12 @@
             dos seus olhos
           </h1>
         </div>
-        <div class="column banner-menu">
+        <!-- <div class="column banner-menu">
           <div class="columns">
             <div class="column has-text-centered">
               <figure>
                 <a href="#">
-                  <img src="<?= get_template_directory_uri(); ?>/assets/images/medico.png" alt="area medicos">
+                  <img src="/assets/images/medico.png" alt="area medicos">
                   <figcaption>
                     <h1 class="title is-4">Médicos</h1>
                     <h2 class="subtitle">Área restrira para profissionais</h2>
@@ -31,7 +31,7 @@
             <div class="column has-text-centered">
               <figure>
                 <a href="#">
-                  <img src="<?= get_template_directory_uri(); ?>/assets/images/agenda.png" alt="agendamento consulta">
+                  <img src="/assets/images/agenda.png" alt="agendamento consulta">
                   <figcaption>
                     <h1 class="title is-4">Pré-agendamento</h1>
                     <h2 class="subtitle">Consultas e exames</h2>
@@ -40,7 +40,7 @@
               </figure>
             </div>
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
   </section>
@@ -54,7 +54,7 @@
             e procedimentos oftalmológicos <br>
             na região do Vale do Paraíba.
           </h1>
-          <a href="" class="button is-info">Clique e conheça mais</a>
+          <a href="<?= get_permalink( get_page_by_path( 'historia' ) ); ?>" class="button is-info">Clique e conheça mais</a>
         </div>
 
         <div class="column has-text-centered-mobile">
