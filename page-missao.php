b2e1a7b8f263407d85bb0684fd678f3f7c35fc10
Rafael <aciolyr@gmail.com>
2017-07-07 15:41:03 -0300

    pagina de missao finalizada

--- page-missao.php
+++ page-missao.php
@@ -1,17 +1,18 @@
 <?php get_header(); ?>
-<div class="hero is-fullheight" id="page-historia">
+<div class="hero is-fullheight" id="page-missao">
 	<div class="hero-nav">
 		<?php get_template_part('components/nav', 'nav'); ?>
 	</div>
 
 	<div class="hero-body">
 		<div class="columns">
-			<div class="column is-5 is-offset-6 content">
+			<div class="column is-4 is-offset-2 content">
 				<?php while(have_posts()) : the_post(); ?>
+					<h1><?php the_title(); ?></h1>
 					<?php the_content(); ?>
 				<?php endwhile; ?>
 			</div>
-			<div class="column color-group is-5 is-offset-6">
+			<div class="column is-4 is-offset-2 color-group">
 				<div class="color-1"></div>
 				<div class="color-2"></div>
 				<div class="color-3"></div>
