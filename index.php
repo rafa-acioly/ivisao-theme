207b447d30a4bd338f34fbe01f3b51d570b8a2c2
Rafeal <aciolyr@gmail.com>
2017-11-02 12:38:01 -0200

    novo layout da home iniciado

--- index.php
+++ index.php
@@ -1,94 +1,77 @@
 <?php get_header(); ?>
 
-<div class="hero is-fullheight">
-  <div class="hero-nav">
-    <?php get_template_part('components/nav', 'nav') ?>
+<div class="hero hero-content-header">
+  <div class="has-text-right">
+    <i class="fa fa-phone fa-fw"></i>12 99750.7888
+    <i class="fa fa-whatsapp fa-fw"></i>12 99750.7888
+    <i class="fa fa-whatsapp fa-fw"></i>12 99767.7888
+    <span class="pipe-separator">|</span>
+    <address>Av. Tivoli, 279 JD. Bethania São José dos Campos</address>
+  </div>
+</div>
+
+<nav class="navbar" role="navigation" aria-label="navegação principal">
+  <div class="navbar-brand">
+    <a href="" class="navbar-item">
+      <img src="http://placehold.it/400x200" alt="logotipo">
+    </a>
   </div>
 
-  <section>
-    <img src="<?= get_template_directory_uri(); ?>/assets/images/IV_SITE_BANNER_HOME_1920X580P_V2.jpg" alt="banner ilustrativo" class="image">
-    <!-- <div class="container">
-      <div class="columns">
-        <div class="column main-title has-text-centered">
-          <h1 class="title is-3">
-            <span>Uma clínica completa</span> para o tratamento dos seus olhos
-          </h1>
-        </div>
-        <div class="column banner-menu">
-          <div class="columns">
-            <div class="column has-text-centered">
-              <figure>
-                <a href="#">
-                  <img src="/assets/images/medico.png" alt="area medicos">
-                  <figcaption>
-                    <h1 class="title is-4">Médicos</h1>
-                    <h2 class="subtitle">Área restrira para profissionais</h2>
-                  </figcaption>
-                </a>
-              </figure>
-            </div>
-            <div class="column has-text-centered">
-              <figure>
-                <a href="#">
-                  <img src="/assets/images/agenda.png" alt="agendamento consulta">
-                  <figcaption>
-                    <h1 class="title is-4">Pré-agendamento</h1>
-                    <h2 class="subtitle">Consultas e exames</h2>
-                  </figcaption>
-                </a>
-              </figure>
-            </div>
-          </div>
-        </div>
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <a href="">Quem somos</a>
       </div>
-    </div> -->
-  </section>
-  <div class="color-group">
-    <div class="color-1"></div>
-    <div class="color-2"></div>
-    <div class="color-3"></div>
-    <div class="color-4"></div>
-  </div>
-  <main class="hero-section">
-    <div class="container">
-      <div class="columns">
-
-        <div class="column vertical-align has-text-centered-mobile is-5">
-          <h1 class="title is-5">
-            A principal clinica para atendimentos
-            e procedimentos oftalmológicos
-            na região do Vale do Paraíba.
-          </h1>
-          <a href="<?= get_permalink( get_page_by_path( 'historia' ) ); ?>" class="button is-info is-default">Clique e conheça mais</a>
-        </div>
-
-        <div class="column has-text-centered-mobile is-6">
-          <h1 class="title is-bolder is-4 is-default">Convênios</h1>
-
-          <div class="image-container slick-convenios">
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-01.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-02.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-03.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-04.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-05.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-06.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-07.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-08.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-09.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-10.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-11.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-12.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-13.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-14.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-15.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-16.jpg" alt=""></figure>
-            <figure><img src="<?= get_template_directory_uri(); ?>/assets/images/convenios/IDV_LOGOS_CONVENIO_90X90P-17.jpg" alt=""></figure>
-          </div>
-        </div>
+      <div class="navbar-item">
+        <a href="">Exames</a>
+      </div>
+      <div class="navbar-item">
+        <a href="">Cirurgias</a>
+      </div>
+      <div class="navbar-item">
+        <a href="">Corpo clinico</a>
+      </div>
+      <div class="navbar-item">
+        <a href="">Convênios</a>
+      </div>
+      <div class="navbar-item">
+        <a href="">Área médica</a>
+      </div>
+      <div class="navbar-item">
+        <a href="">Pré-agendamento</a>
+      </div>
+      <div class="navbar-item">
+        <a href="">Contato</a>
       </div>
     </div>
-  </main>
-  <?php get_template_part('components/hero', 'footer'); ?>
-</div>
+  </div>
+</nav>
+
+<main class="hero hero-content-body">
+  <div class="hero-side-menu">
+    <div class="hero-menu-item">
+      <div class="hero-menu-img">
+        <img src="http://placehold.it/100x100" alt="">
+      </div>
+      <div class="hero-menu-content has-text-centered">
+        <h2 class="title is-5">Área médica</h2>
+        <h3 class="subtitle is-6">Área restrira para profissionais</h3>
+      </div>
+    </div>
+    <div class="hero-menu-item">
+      <div class="hero-menu-img">
+        <img src="http://placehold.it/100x100" alt="">
+      </div>
+      <div class="hero-menu-content has-text-centered">
+        <h2 class="title is-5">Pré-agendamento</h2>
+        <h3 class="subtitle is-6">Consulta e exames</h3>
+      </div>
+    </div>
+  </div>
+  <figure>
+    <img src="https://source.unsplash.com/collection/1128928/1600x900" alt="">
+  </figure>
+</main>
+
 </body>
 </html>
