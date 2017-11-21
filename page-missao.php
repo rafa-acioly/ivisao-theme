5e85d5239d0f29c9fc4372c130a269565276733e
Rafeal <aciolyr@gmail.com>
2017-11-21 21:44:51 -0200

    pagina de missao visao e valores finalizada

--- page-missao.php
+++ page-missao.php
@@ -7,21 +7,21 @@
 	<div class="hero-body">
 		<div class="container">
 			<div class="columns">
-				<div class="column is-4 is-offset-1 content">
+				<div class="column is-6 is-offset-1 content">
 					<?php while(have_posts()) : the_post(); ?>
 						<h1><?php the_title(); ?></h1>
 						<?php the_content(); ?>
 					<?php endwhile; ?>
 				</div>
-				<div class="column is-4 is-offset-1 color-group">
+				<div class="column is-6 is-offset-1 color-group">
 					<div class="color-1"></div>
 					<div class="color-2"></div>
-					<div class="color-3"></div>
 					<div class="color-4"></div>
+					<div class="color-3"></div>
 				</div>
 			</div>
 		</div>
-	</div>
+	</div>''
 
 	<?php get_template_part('components/hero', 'footer'); ?>
 </div>
